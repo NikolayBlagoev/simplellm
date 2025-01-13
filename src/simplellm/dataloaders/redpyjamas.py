996e4db7045a5c555db26ad2ba16c0f9983bfbd6
NikolayBlagoev <workerthreadllc@gmail.com>
2025-01-13 19:10:29 +0200

    test

--- src/simplellm/dataloaders/redpyjamas.py
+++ src/simplellm/dataloaders/redpyjamas.py
@@ -2,12 +2,14 @@ from itertools import cycle
 import torch
 from datasets import load_dataset
 from simplellm.tokenizers.abstracttokenizer import AbstractTokenizer
+from torch.utils.data import DataLoader, IterableDataset
 from .abstract_dataset import AbstractDataset
 class RedPyjama(object):
     
 
-    def __init__(self, tokenizer: AbstractTokenizer, batch_size = 5_000, seq_l=2048,num_workers=0):
-        dataset = load_dataset("togethercomputer/RedPajama-Data-1T", "c4", trust_remote_code=True,streaming=True)
+    def __init__(self, tokenizer: AbstractTokenizer, streaming = True, batch_size = 5_000, seq_l=2048, split = 'train',num_workers=0):
+        dataset = load_dataset("togethercomputer/RedPajama-Data-1T", "default", streaming = streaming, trust_remote_code=True)
+        
         iterable_dataset = dataset.shuffle(buffer_size=10_000)
         iterable_dataset = iterable_dataset.map(self.tokenization, batched=True, batch_size=batch_size)
         self.batch_size = batch_size
@@ -16,8 +18,9 @@ class RedPyjama(object):
         self.dl = torch.utils.data.DataLoader(self.iterable_dataset,batch_size=batch_size,shuffle=False, num_workers=num_workers,pin_memory=True,collate_fn=None)
         self.tokenizer = tokenizer
         self.seq_l = seq_l
-        print("RedPajama DATASET LOADED...")
-    
+        print("WIKIPEDIA DATASET LOADED...")
+    def tokenization(self, t):
+        return {"text": self.tokenizer.encode(t["text"])}
     def get_data(self):
         return self.dl
     
@@ -33,4 +36,5 @@ class RedPyjama(object):
         return cycle(self.get_data())
     def __iter__(self):
         return cycle(self.get_data())
+
 
