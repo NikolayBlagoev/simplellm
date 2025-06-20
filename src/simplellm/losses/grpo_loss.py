96cb6d24b5965b04ae172f895a7baebf82fa440d
NikolayBlagoev <workerthreadllc@gmail.com>
2025-06-20 17:19:20 +0300

    remove self`

--- src/simplellm/losses/grpo_loss.py
+++ src/simplellm/losses/grpo_loss.py
@@ -4,7 +4,7 @@ import torch
 
 def grpo_loss(log_probs: torch.Tensor, original_log_probs: torch.Tensor, rewards: torch.Tensor,  msk: torch.Tensor = None, kl = 0, eps = 0.26, kl_weight = 0.01, reduction = "mean"):
     r = (log_probs - original_log_probs).exp()
-    loss = -torch.min(r * rewards, r.clamp(1 - self.eps, 1 + self.eps) * rewards)
+    loss = -torch.min(r * rewards, r.clamp(1 - eps, 1 + eps) * rewards)
     loss += kl_weight * kl
     if msk is None:
         loss = msk.mean(dim=-1)
