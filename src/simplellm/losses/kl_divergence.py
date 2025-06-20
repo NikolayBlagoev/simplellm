851864d4b0b506466f68a1d1806de5a7ffa39527
NikolayBlagoev <workerthreadllc@gmail.com>
2025-06-20 15:42:26 +0300

    grpo fix

--- src/simplellm/losses/kl_divergence.py
+++ src/simplellm/losses/kl_divergence.py
@@ -4,7 +4,7 @@ import torch
 def kl_loss(x: torch.Tensor, target: torch.Tensor, mask: torch.Tensor = None):
     r = target.float() - x.float()
     if mask is not None:
-        r = r * action_mask
+        r = r * mask
     return r.exp() - r - 1
 
 
