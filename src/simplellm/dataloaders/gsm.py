65ae9354b327cac45a5b940f8d7bf6954851a1c5
NikolayBlagoev <workerthreadllc@gmail.com>
2025-06-23 12:19:31 +0300

    pre-prompt

--- src/simplellm/dataloaders/gsm.py
+++ src/simplellm/dataloaders/gsm.py
@@ -10,15 +10,15 @@ class GSM(_AbstractDataset):
     
 
     def __init__(self, tokenizer: AbstractTokenizer, streaming = True, batch_size = 5_000, seq_l=2048, split = 'train',num_workers=0, skip = 0, 
-                    dataset_type = PretrainDataset):
+                    pre_prompt = "", dataset_type = PretrainDataset):
         dataset = load_dataset("openai/gsm8k", "main", split=split,streaming = streaming, trust_remote_code=True)
-        
+        self.pre_prompt = pre_prompt
         super().__init__(dataset,tokenizer,batch_size,seq_l,num_workers,skip,dataset_type)
     def tokenization(self, t):
-        return {"text": self.tokenizer.encode([t["question"][0] + t["answer"][0]])}
+        return {"text": self.tokenizer.encode([self.pre_prompt + t["question"][0] + t["answer"][0]])}
 
     def completion_tokenization(self,t):
         # print(t)
-        return {"prompt": self.tokenizer.encode(t["question"]), "completion": self.tokenizer.encode(t["answer"])}
+        return {"prompt": self.tokenizer.encode(self.pre_prompt + t["question"]), "completion": self.tokenizer.encode(t["answer"])}
 
 
