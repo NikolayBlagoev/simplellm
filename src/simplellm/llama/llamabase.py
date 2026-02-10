8f636e150053df366449a56c4d12e4f5e991a460
NikolayBlagoev <workerthreadllc@gmail.com>
2026-02-10 18:41:12 +0200

    neeed o shapes

--- src/simplellm/llama/llamabase.py
+++ src/simplellm/llama/llamabase.py
@@ -203,7 +203,7 @@ class Attention(nn.Module):
                 v=xv.to(torch.bfloat16), 
                 causal=True,
             )[0].to(torch.float32)
-            # print(o.shape)
+            print(o.shape)
         else:
             xk = repeat_intrleave(xk, self.num_heads // self.n_kv_heads)
             xv = repeat_intrleave(xv, self.num_heads // self.n_kv_heads)
@@ -219,7 +219,7 @@ class Attention(nn.Module):
                 scale = self.scaling
             ).transpose(1, 2).contiguous()
         o = o.reshape(bsz, seqlen, -1).contiguous()
-        # print(o.shape)
+        print(o.shape)
         o = self.o_proj(o)
         
         return o
